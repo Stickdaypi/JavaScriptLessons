f6832c49317d068166cbd18b067af42e5f28b861
Anatoliy <stickdaypi@gmail.com>
2022-09-29 13:42:07 +0200

    cycle in cycle

--- js/script.js
+++ js/script.js
@@ -40,3 +40,33 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log (personalMovieDB);
+
+// console.log( 1 && 2 && 3 );
+ 
+// console.log( !1 && 2 || !3 );
+ 
+// console.log( 25 || null && !3 );
+ 
+// console.log( NaN || null || !3 || undefined || 5);
+ 
+// console.log( NaN || null && !3 && undefined || 5);
+ 
+// console.log( 5 === 5 && 3 > 1 || 5);
+ 
+let age = 10;
+
+for (let i = age; i <= 18; i++ ){
+    if (i<18) {
+        let w = 18-i;    
+    console.log('You need birthday certificate and wait for ' + w + ' years!');
+    console.log(i);}
+    
+    else {
+    for (let j = i; j <= 60; j++ ){
+        let d = 60-j;    
+        console.log('You need passport and wait for the rest ' + d + ' years!');        
+    }}
+    
+}
+
+
